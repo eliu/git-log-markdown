975917268ca0dac0c736915b0f54344d8ab9b73b
eliu <eliuhy@163.com>
2023-11-27 20:55:49 +0800

    Optimize text output and Change function name

--- PSGitLogMarkdown/PSGitLogMarkdown.psd1
+++ PSGitLogMarkdown/PSGitLogMarkdown.psd1
@@ -5,7 +5,7 @@ ModuleVersion = '0.0.1'
 GUID = '1ba7ff5a-7ad9-416f-966c-78af471a17eb'
 Author = 'Liu Hongyu'
 Copyright = '(c) Liu Hongyu. All rights reserved.'
-FunctionsToExport = @("Write-GitLog")
+FunctionsToExport = @("ConvertFrom-GitLog")
 CmdletsToExport = @()
 AliasesToExport = @()
 }
--- PSGitLogMarkdown/PSModule.psm1
+++ PSGitLogMarkdown/PSModule.psm1
@@ -1,11 +1,9 @@
-<#
-#>
-
 $script:CommitRange
 $script:OutFileName
-$script:OutFormat
+$script:NullDevice = ($PSVersionTable.Platform -like "WIN*") ? "NUL" : "/dev/null"
+$script:CurrentDirectory = Split-Path -Leaf -Path .
 
-function Write-GitLog
+function ConvertFrom-GitLog
 {
     [CmdletBinding()]
     Param
@@ -48,9 +46,15 @@ function Write-GitLog
             $script:OutFileName = "CHANGELOG-$Since-$Until.md"
         }
 
-        Write-Verbose "The final commit range is ${script:CommitRange}"
-        Write-Verbose "Out file name is ${script:OutFileName}"
-        Write-Verbose "Output format is ${script:OutFormat}"
+        if ($VerbosePreference -eq [System.Management.Automation.ActionPreference]::Continue)
+        {
+            $VerboseOutput = @{}
+            $VerboseOutput.Add("COMMIT RANGE"  , ${script:CommitRange})
+            $VerboseOutput.Add("FILE NAME"     , ${script:OutFileName})
+            $VerboseOutput.Add("GIT REPO"      , ${script:CurrentDirectory})
+            $VerboseOutput.Add("OUTPUT FORMAT" , $Format)
+            $VerboseOutput | Format-Table
+        }
     }
 
     Process
@@ -58,7 +62,7 @@ function Write-GitLog
         if ($Format -eq "markdown")
         {
             $gitlog = Invoke-Expression `
-            'git log --no-merges --format="| %h | %ad | %an | %s |" --date=short ${script:CommitRange} 2>/dev/null || true'
+            'git log --no-merges --format="| %h | %ad | %an | %s |" --date=short ${script:CommitRange} 2> ${script:NullDevice}'
 
             Write-Verbose "Writing file $script:OutFileName..."
             Write-Output "## changelog for git repo: ``$(Get-Location | Split-Path -Leaf)``" > $script:OutFileName
@@ -66,7 +70,42 @@ function Write-GitLog
             Write-Output "| --- | --- | --- | --- |" >> $script:OutFileName
             Write-Output $gitlog >> $script:OutFileName
         }
+
+        if ($Format -eq 'text')
+        {
+            $NativeCommand = "git log --no-merges --format=""%h|%ad|%an|%s|"" --date=short ${script:CommitRange} 2> ${script:NullDevice}"
+            Write-Debug "Git command -> $NativeCommand"
+            $GitLog = Invoke-Expression $NativeCommand
+            Write-Debug "Output result ->"
+            Write-Debug "$GitLog"
+            Write-Output "## changelog for git repo: ${script:CurrentDirectory}"
+            $GitLog | ConvertFrom-Csv -Delimiter '|' -Header @("COMMIT ID", "DATE", "AUTHOR", "COMMIT MSG")
+        }
     }
+    <#
+    .SYNOPSIS
+    ConvertFrom-GitLog convert git-log to markdown format.
+
+    .DESCRIPTION
+
+    .PARAMETER Since
+    Commit ID that was extracted from
+
+    .PARAMETER Until
+    Commit ID that was extracted to
+
+    .EXAMPLE
+    # Get git log between v1.0.0 and v1.0.1
+    ConvertFrom-GitLog -Since v1.0.0 -Until v1.0.1
+
+    .EXAMPLE
+    # Pring got log from the beginning to v1.0.0
+    ConvertFrom-GitLog -Until v1.0.0
+
+    .EXAMPLE
+    # Print git log from v1.0.1 to the latest commit
+    ConvertFrom-GitLog -Since v1.0.1
+    #>
 }
 
-Export-ModuleMember Write-GitLog
+Export-ModuleMember ConvertFrom-GitLog
